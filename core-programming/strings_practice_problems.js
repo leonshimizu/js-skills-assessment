ccf5cf6b64aa704ea95293ada72ac24c8a717259
Leon <lmshimizu@gmail.com>
2021-11-02 23:22:11 -0700

    started hash practice problems

---
+++ core-programming/strings_practice_problems.js
@@ -0,0 +1,50 @@
+// 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// var first = "Stassie";
+// var last = "Shimizu";
+
+// console.log(first + " " + last);
+
+// 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+// console.log(`${first} ${last}`);
+
+// 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// doesn't work in terminal, online it says it will work in the browser
+
+// var word = window.prompt("Enter a word:");
+// if (word === "marco") {
+//   console.log("polo");
+// }
+
+// var word = window.prompt("Enter any word here:");
+
+// if (word === "marco") {
+//   console.log("polo");
+// }
+
+// 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// var c1 = "red";
+// var c2 = "white";
+// var c3 = "blue";
+
+// console.log(c1 + " " + c2 + " " + c3);
+
+// 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+// console.log(`${c1} ${c2} ${c3}`);
+
+// 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+// 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+// 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+// SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
