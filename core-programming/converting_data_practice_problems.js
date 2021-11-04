c01347ec694dd9beb246af683ed679269cc0f34a
“leonshimizu” <lmshimizu@gmail.com>
2021-11-03 22:22:56 -0700

    continuing converting data practice problems

--- core-programming/converting_data_practice_problems.js
+++ core-programming/converting_data_practice_problems.js
@@ -1,7 +1,15 @@
 // 1. Convert an array of arrays into a hash.
 //    For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
+var array = [[1, 3], [8, 9], [2, 16]];
+var hash = {};
 
+array.forEach(function(number) {
+
+});
+
+// var hash = array.map(a => Object.fromEntries(a));
+// console.log(hash);
 
 // 2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 //    For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
