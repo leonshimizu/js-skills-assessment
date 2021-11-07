01aa8d2e6c3b23b09ee655662a700c27482a35e1
“leonshimizu” <lmshimizu@gmail.com>
2021-11-07 06:55:05 -0800

    completed converting data practice problems

--- converting_data_practice_problems.js
+++ converting_data_practice_problems.js
@@ -112,11 +112,28 @@
 // 9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 //    For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
+// var array = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}, {author: "George Orwell", title: "test"}];
+// var hash = {};
 
+// array.forEach(function(book) {
+//   var author = Object.entries(book)[0][1];
+//   var title = Object.entries(book)[1][1];
+//   if (hash[author] === undefined) {
+//     hash[author] = [];
+//   }
+//   hash[author].push(title);
+// });
+// console.log(hash);
 
 // 10. Given a hash, create a new hash that has the keys and values switched.
 //    For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
 
+// var hash = {a: 1, b: 2, c: 3};
+// var newHash = {};
 
+// Object.entries(hash).forEach(function(array) {
+//   newHash[array[1]] = array[0];
+// });
+// console.log(newHash);
 
 //SOLUTIONS: https://gist.github.com/peterxjang/216a7a6e8411ee5c05118e78022f2bc7
